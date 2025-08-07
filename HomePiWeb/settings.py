66ebec349d4fcc785103adad168f9ae34d7d1df4
qkauia-guy <kauia407@gmail.com>
2025-08-08 01:43:42 +0800

    feat(users): add user templates and update views, urls, and settings

--- HomePiWeb/settings.py
+++ HomePiWeb/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "HomePiWeb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
