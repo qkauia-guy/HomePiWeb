f984e3c7b5c1ee317f1e94615caba3b49a362956
qkauia-guy <kauia407@gmail.com>
2025-08-07 16:05:20 +0800

    chore: update settings.py Time Zone and Language

--- HomePiWeb/settings.py
+++ HomePiWeb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@1cytfof3ry-@pl+#-t@sqconfp)ec87&a($qn9l-lo#*e^8@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # 上線改 False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] # 開發時使用
 
 
 # Application definition
@@ -102,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
