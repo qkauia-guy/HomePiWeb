f3bf43bbafda858ac9685446713c6572202e5d57
qkauia-guy <kauia407@gmail.com>
2025-08-11 22:57:21 +0800

    feat(users): move user-related routes into users/urls.py and update project urls

--- HomePiWeb/urls.py
+++ HomePiWeb/urls.py
@@ -1,20 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from users import views
-from users.views import login_view, home_view
 from django.shortcuts import redirect
 
-
 urlpatterns = [
-    path("admin/", admin.site.urls),  # Django 後台
-    path("register/", views.register_view, name="register"),  # 註冊
-    path("login/", login_view, name="login"),
-    path("home/", home_view, name="home"),
+    path("admin/", admin.site.urls),
+    # 使用者相關路由（register/login/home + 密碼重設）
+    path("", include("users.urls")),
+    path("", include("pi_devices.urls")),
     path(
         "",
         lambda request: (
             redirect("home") if request.user.is_authenticated else redirect("login")
         ),
+        name="index",
     ),
-    path("", include("pi_devices.urls")),
 ]
